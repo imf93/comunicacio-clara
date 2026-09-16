e6b7da46846af1a01ffa3c164e6af97b0b0a4ad4
Ivan Merino <imerinofajardo@gmail.com>
2026-09-16 07:28:10 +0200

    Add Catalan live word cloud for GitHub Pages with Firebase and Excel export

---
+++ docs/firebase-config.js
@@ -0,0 +1,5 @@
+// Configuracion publica de la app Firebase. No contiene contrasenas.
+window.PALABRA_FIREBASE = {
+  "apiKey": "AIzaSyBZ914QDzKhOy-tPf8Irmua1w6nL2-VWWk",
+  "databaseURL": "https://palabra-viva-b0c26-default-rtdb.europe-west1.firebasedatabase.app"
+};
